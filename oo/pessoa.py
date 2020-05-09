77b7281da977096cdac264f55aba7388d3e40671
VAIO-MC3 <arc.mau@gmail.com>
2020-05-09 20:10:12 -0300

    Criado e removido atributo dinâmico de objeto de tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -18,3 +18,9 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Ramalho'  # Atributos dinâmicos
+    print(luciano.sobrenome)
+    print(luciano.__dict__)
+    del luciano.filhos        # Atributos de instância fica presente na  __dict__
+    print(luciano.__dict__)
+    print(renzo.__dict__)
