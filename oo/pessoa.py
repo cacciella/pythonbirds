7c3d751b22df2db6ca6015460ac5d6b45f909aed
VAIO-MC3 <arc.mau@gmail.com>
2020-05-11 10:11:42 -0300

    Implementação do Carro

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -11,7 +11,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, o meu nome é {self.nome}'
 
     @staticmethod   # cria um metodo independente do objeto a ser executado    (@ - decorator)
     def metodo_estatico():
@@ -22,7 +22,9 @@ class Pessoa:
         return f'{cls} - olhos {cls.olhos}'
 
 class Homem(Pessoa):
-    pass
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()     # super(). acesso os elementos da classe Pai
+        return f'{cumprimentar_da_classe}. Aperto de mão'
 
 class Mutante(Pessoa):      # sobrescrita de atributo de dados /classe
     olhos = 3
@@ -62,3 +64,5 @@ if __name__ == '__main__':
     print(isinstance(renzo, Pessoa))
     print(isinstance(renzo, Homem))
     print(renzo.olhos)
+    print(luciano.cumprimentar())
+    print(renzo.cumprimentar())
