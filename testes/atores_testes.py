9b6ced7b72073fdef4b4b0591825a331a5759541
Mauricio Cacciella <62062182+cacciella@users.noreply.github.com>
2020-05-15 15:28:04 -0300

    Update atores_testes.py

--- testes/atores_testes.py
+++ testes/atores_testes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+#
 from __future__ import unicode_literals
 from unittest.case import TestCase
 from atores import Ator, DESTRUIDO, ATIVO, Obstaculo, Porco, PassaroAmarelo, PassaroVermelho
